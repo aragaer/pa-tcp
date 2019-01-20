117b99999073c0c7e5c41db9fba8bdcae24c72d9
aragaer <aragaer@gmail.com>
2019-01-20 21:21:18 +0300

    Extract Channel to a separate file

---
+++ src/channel.rs
@@ -0,0 +1,59 @@
+extern crate serde_json;
+
+use bytes::{BufMut, BytesMut};
+use mio::tcp::TcpStream;
+use serde_json::Value;
+use std::io;
+use std::io::Read;
+
+pub struct Channel {
+    pub socket: TcpStream,
+    in_buffer: BytesMut,
+}
+
+impl Channel {
+    pub fn new(socket: TcpStream) -> Channel {
+        Channel {
+            socket: socket,
+            in_buffer: BytesMut::with_capacity(2048),
+        }
+    }
+    pub fn read(self: &mut Channel) -> io::Result<Vec<Value>> {
+        let buffer = &mut self.in_buffer;
+        loop {
+            match unsafe {
+                self.socket.read(buffer.bytes_mut())
+            } {
+                Ok(0) => {
+                    break;
+                }
+                Ok(count) => unsafe {
+                    buffer.advance_mut(count);
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    // Socket is not ready anymore, stop reading
+                    break;
+                }
+                e => panic!("err={:?}", e), // Unexpected error
+            }
+        }
+        if buffer.is_empty() {
+            println!("Socket closed!");
+            Err(io::Error::new(io::ErrorKind::ConnectionReset, "Socket closed"))
+        } else {
+            let mut result: Vec<Value> = Vec::new();
+            let mut total_len = 0;
+            for piece in buffer.split(|b| *b == b'\n') {
+                let new_total = total_len + piece.len() + 1;
+                if new_total > buffer.len() {
+                    break;
+                }
+                result.push(serde_json::from_slice(piece).unwrap());
+                total_len = new_total;
+            }
+            buffer.advance(total_len);
+            Ok(result)
+        }
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,21 @@ extern crate bytes;
 extern crate encoding;
 extern crate getopts;
 extern crate mio;
-extern crate serde;
 extern crate serde_json;
 extern crate slab;
 
-use bytes::{BufMut, BytesMut};
 use encoding::{Encoding, ByteWriter, EncoderTrap};
 use encoding::types::RawEncoder;
 use encoding::all::ASCII;
-use getopts::Options;
 use mio::*;
 use mio::tcp::{TcpListener, TcpStream};
 use serde_json::Value;
-use slab::Slab;
-use std::env;
 use std::io;
-use std::io::Read;
 use std::io::Write;
 use std::vec::Vec;
 
+mod channel;
 
-// hexadecimal numeric character reference replacement
 fn hex_escape(_encoder: &mut RawEncoder, input: &str, output: &mut ByteWriter) -> bool {
     for escape in input.chars().map(|ch| format!("\\u{:04X}", ch as isize)) {
         output.write_bytes(escape.as_bytes());
@@ -32,72 +26,19 @@ fn hex_escape(_encoder: &mut RawEncoder, input: &str, output: &mut ByteWriter) -
 
 static HEX_ESCAPE: EncoderTrap = EncoderTrap::Call(hex_escape);
 
-pub struct Channel {
-    socket: TcpStream,
-    in_buffer: BytesMut,
-}
-
-impl Channel {
-    pub fn new(socket: TcpStream) -> Channel {
-        Channel {
-            socket: socket,
-            in_buffer: BytesMut::with_capacity(2048),
-        }
-    }
-}
-
-fn read(channel: &mut Channel) -> std::io::Result<Vec<String>> {
-    let mut socket = channel.socket.try_clone()?;
-    let buffer = &mut channel.in_buffer;
-    loop {
-        match unsafe {
-            socket.read(buffer.bytes_mut())
-        } {
-            Ok(0) => {
-                break;
-            }
-            Ok(count) => unsafe {
-                buffer.advance_mut(count);
-            }
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                // Socket is not ready anymore, stop reading
-                break;
-            }
-            e => panic!("err={:?}", e), // Unexpected error
-        }
-    }
-    if buffer.is_empty() {
-        println!("Socket closed!");
-        Err(io::Error::new(io::ErrorKind::ConnectionReset, "Socket closed"))
-    } else {
-        let mut result: Vec<String> = Vec::new();
-        let mut total_len = 0;
-        for piece in buffer.split(|b| *b == b'\n') {
-            let new_total = total_len + piece.len() + 1;
-            if new_total > buffer.len() {
-                break;
-            }
-            result.push(String::from_utf8_lossy(piece.as_ref()).to_string());
-            total_len = new_total;
-        }
-        buffer.advance(total_len);
-        Ok(result)
-    }
-}
-
-fn serve(router: &str, sock: &str, prefix: &str) -> std::io::Result<()> {
+fn serve(router: &str, sock: &str, prefix: &str) -> io::Result<()> {
     let listener = TcpListener::bind(&sock.parse().expect("Wrong format for socket address"))?;
     let mut stream = TcpStream::connect(&router.parse().expect("Wrong format for router address"))?;
     stream.write(prefix.as_bytes())?;
     stream.write(b"\n")?;
 
     let poll = Poll::new()?;
-    let mut channels = Slab::new();
+    let mut channels = slab::Slab::new();
 
     const SERVER: Token = Token(0);
     const ROUTER: Token = Token(1);
 
-    channels.insert(Channel::new(stream.try_clone()?));
+    channels.insert(channel::Channel::new(stream.try_clone()?));
 
     poll.register(&listener, SERVER, Ready::readable(), PollOpt::level())?;
     poll.register(&stream, ROUTER, Ready::readable(), PollOpt::level())?;
@@ -112,15 +53,14 @@ fn serve(router: &str, sock: &str, prefix: &str) -> std::io::Result<()> {
                 SERVER => {
                     let (client, addr) = listener.accept().expect("Accept success");
                     println!("Got connection from {}", addr);
-                    let channel = Channel::new(client.try_clone()?);
+                    let channel = channel::Channel::new(client.try_clone()?);
                     let t: Token = Token(channels.insert(channel) + 1);
                     poll.register(&client, t, Ready::readable(), PollOpt::level())?;
                 }
                 ROUTER => {
-                    match read(&mut channels[0]) {
-                        Ok(strings) => {
-                            for s in strings {
-                                let mut message: Value = serde_json::from_str(&*s).unwrap();
+                    match channels[0].read() {
+                        Ok(messages) => {
+                            for mut message in messages {
                                 let channel = message["to"]["channel"].clone();
                                 let parts: Vec<&str> = channel.as_str().unwrap().splitn(3, ':').collect();
                                 message["to"]["channel"] = Value::String(String::from(parts[2]));
@@ -136,12 +76,10 @@ fn serve(router: &str, sock: &str, prefix: &str) -> std::io::Result<()> {
                     }
                 }
                 Token(token) => {
-                    println!("Got event with token {}", token);
                     let idx = token - 1;
-                    match read(&mut channels[idx]) {
-                        Ok(strings) => {
-                            for s in strings {
-                                let mut message: Value = serde_json::from_str(&*s).unwrap();
+                    match channels[idx].read() {
+                        Ok(messages) => {
+                            for mut message in messages {
                                 let new_from = format!("{}:{}:{}", prefix, idx, message["from"]["channel"].as_str().unwrap());
                                 message["from"]["channel"] = Value::String(new_from);
                                 let msg_str = format!("{}\n", serde_json::to_string(&message).unwrap());
@@ -160,9 +98,9 @@ fn serve(router: &str, sock: &str, prefix: &str) -> std::io::Result<()> {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
 
-    let mut opts = Options::new();
+    let mut opts = getopts::Options::new();
     opts.reqopt("r", "router-socket", "router address", "HOST:PORT");
     opts.reqopt("s", "socket", "socket to listen to", "HOST:PORT");
     opts.optopt("p", "prefix", "router prefix, default \"tcp\"", "PREFIX");
